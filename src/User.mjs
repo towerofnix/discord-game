231e36c6772c9b2dad87ecafcb4f652d63aeb559
alex <16batesa@gmail.com>
2017-09-23 02:14:10 +0100

    Implement basic database integration & barebones User document
    Make sure you install `nedb` or `mongodb`! Most likely NeDB for production, though

--- src/User.mjs
+++ src/User.mjs
@@ -1,5 +1,52 @@
-import Discord from 'discord.js'
+import discord from 'discord.js'
+import camo from 'camo'
 
-export default class User {
+export async function getMemberById(id) {
+  if (!id || typeof id !== 'string') throw 'getMemberById(string id) expected'
 
+  // discord.Guild#fetchMember has a cache, so we don't need to memoize this
+  return await global.guild.fetchMember(id, true) // discord.GuildMember
+}
+
+export default class User extends camo.Document {
+  constructor() {
+    super()
+
+    this._id = { type: String, unique: true, required: true } // discord.Snowflake
+    this.currentRoom = { type: String, default: 'void' } // reference to Room (TODO)
+  }
+
+  static async getById(id) {
+    if (!id || typeof id !== 'string') throw 'User#getById(string id) expected'
+    return await User.findOne({ _id: id })
+  }
+
+  static async exists(id) {
+    if (!id || typeof id !== 'string') throw 'User#exists(string id) expected'
+    return await User.getById(id) !== null
+  }
+
+  static async addNewUsers() {
+    let numAdded = 0
+
+    for (const [ id, member ] of global.guild.members) {
+      // if this user is a bot, ignore it
+      if (member.user.bot === true)
+        continue
+
+      // is this user in the database?
+      if (await User.exists(id) === false) {
+        // add the user to the database
+        let user = User.create({ _id: id })
+        await user.save()
+        numAdded++
+      }
+    }
+
+    return numAdded
+  }
+
+  async getName() {
+    return (await getMemberById(this._id)).displayName
+  }
 }
--- src/main.mjs
+++ src/main.mjs
@@ -3,6 +3,7 @@ import Discord from 'discord.js'
 import { log } from './util'
 import env from './env'
 import chalk from 'chalk'
+import camo from 'camo'
 
 process.on('uncaughtException', async err => {
   if (typeof err === 'object') await log.inspect(err) // only shows on -1 loglevel [debug]
@@ -14,14 +15,39 @@ process.on('unhandledRejection', async err => {
   await log.fatal(err)
 })
 
-const client = new Discord.Client
+global.client = new Discord.Client
 
 client.on('ready', async () => {
   await log.success('Connected to Discord API')
+
+  global.guild = client.guilds.first() // assuming one and only!
+
+  // New user check
+  await log.info('Checking for any new users not yet in the database...')
+  const newUserAmount = await User.addNewUsers()
+  if (newUserAmount > 0)
+    await log.success(`Added (${newUserAmount}) new users to the database`)
+  else
+    await log.info('No users to add')
 })
 
-env('discord_client_id', 'string').then(clientId => {
-  const perms = 0x00000008 // ADMINISTRATOR; bitwise OR with others if need be
-  return log.info(chalk`Add to server url: {underline {cyan https://discordapp.com/oauth2/authorize?&client_id=${clientId}&scope=bot&permissions=${perms}&response_type=code}}`)
-}).then(() => env('discord_token', 'string'))
-  .then(token => client.login(token))
+client.on('guildMemberAdd', async member => {
+  if (await User.exists(member.id) === false) { // quick sanity check
+    // Add new user to the database
+    await log.info('A new user just joined! Adding them to the database...')
+    const user = User.create({ _id: member.id })
+    await user.save()
+    await log.success(chalk`Added user: {cyan ${await user.getName()}}`)
+  }
+})
+
+env('database_uri', 'string', 'nedb://data/')
+  .then(dbUri => camo.connect(dbUri))
+  .then(() => log.success('Connected to database'))
+  .then(() => env('discord_client_id', 'string'))
+  .then(clientId => {
+    const perms = 0x00000008 // ADMINISTRATOR; bitwise OR with others if need be
+    return log.info(chalk`Add to server url: {underline {cyan https://discordapp.com/oauth2/authorize?&client_id=${clientId}&scope=bot&permissions=${perms}&response_type=code}}`)
+  })
+  .then(() => env('discord_token', 'string'))
+    .then(token => client.login(token))
