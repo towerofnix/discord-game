429017ffdc31a89c24abd0d0c89d1d8a62613765
Florrie <towerofnix@gmail.com>
2017-10-17 11:10:40 -0300

    Change around music
    I'm opinionated and I think this music is better.
    
    Did you know Git thinks that I *renamed* lonely-void.mp3, the old bitsy
    song, to the totally new truth-in-the-stones.mp3 file? Because
    apparently I didn't rewrite *enough* of the old song's bits for Git to
    think this is a new file!

--- src/game/music/index.js
+++ src/game/music/index.js
@@ -1,9 +1,18 @@
 import path from 'path'
 
+// 8-bit-style music:
 // https://www.youtube.com/playlist?list=PLBE459782E55DE0D8
+
+// General royalty free music:
+/*
+  Kevin MacLeod (incompetech.com)
+  Licensed under Creative Commons: By Attribution 3.0 License
+  http://creativecommons.org/licenses/by/3.0/
+*/
+
 // Voodoo webpack magic! No hacks, though. Fun!
-import battle from './battle.mp3'
-import lonelyVoid from './lonely-void.mp3'
+import battle from './mountain-emperor.mp3' // incompetech
+import lonelyVoid from './truth-in-the-stones.mp3' // incompetech
 
 const music = [
   [ 'battle', battle ],
