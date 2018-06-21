70a084fa10438daa47562a7df8c0cf4d9b066c68
Florrie <towerofnix@gmail.com>
2018-06-21 08:31:40 -0300

    More fun

--- src/lib/FakeDiscordTelnetServerClient.js
+++ src/lib/FakeDiscordTelnetServerClient.js
@@ -4,7 +4,62 @@ import EventEmitter from 'events'
 
 const __guild__ = Symbol()
 
+class DiscordArray extends Array {
+  first() { return this[0] }
+  last() { return this[this.length - 1] }
+}
+
+class DiscordMap extends Map {
+  entries() {
+    return Array.from(super.entries())
+  }
+
+  filter(cb) {
+    return new DiscordMap(this.entries().filter(([ k, v ]) => cb(v)))
+  }
+
+  first() {
+    return this.values()[0]
+  }
+
+  _find(index, propOrCb, value) {
+    let ret
+    if (typeof propOrCb === 'function') {
+      ret = this.entries().find(propOrCb)
+    } else {
+      ret = this.entries().find(v => v[propOrCb] === value)
+    }
+    if (ret) {
+      return ret[index]
+    } else {
+      return ret
+    }
+  }
+
+  find(propOrCb, value) {
+    return this._find(1, propOrCb, value)
+  }
+
+  findKey(propOrCb, value) {
+    return this._find(0, propOrCb, value)
+  }
+
+  map(cb) {
+    return new DiscordMap(this.entries().map(([ k, v ]) => cb(v)))
+  }
+
+  values() {
+    // Maybe not spec-compilant
+    return Array.from(super.values())
+  }
+}
+
 class FakeGuild {
+  constructor() {
+    this.channels = new DiscordMap()
+    this.members = new DiscordMap()
+    this.roles = new DiscordMap()
+  }
 }
 
 export default class FakeDiscordTelnetServerClient extends EventEmitter {
@@ -13,6 +68,10 @@ export default class FakeDiscordTelnetServerClient extends EventEmitter {
 
     this[__guild__] = new FakeGuild()
 
-    this.guilds = {first: () => this[__guild__]}
+    this.guilds = new DiscordArray(this[__guild__])
+
+    setTimeout(() => {
+      this.emit('ready')
+    }, 50)
   }
 }
